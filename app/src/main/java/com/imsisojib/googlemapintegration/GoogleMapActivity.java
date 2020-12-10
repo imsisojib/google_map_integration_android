86b3aa100e155021b7a583043f9638121139b8a2
Md Sirajul Islam Sojib <sirajulislam1123@gmail.com>
2020-12-10 22:06:21 +0600

    Places SDK added & Current-Location-Button position aligned in bottom.

--- app/src/main/java/com/imsisojib/googlemapintegration/GoogleMapActivity.java
+++ app/src/main/java/com/imsisojib/googlemapintegration/GoogleMapActivity.java
@@ -1,28 +1,73 @@
 package com.imsisojib.googlemapintegration;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.libraries.places.api.model.AutocompletePrediction;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
-public class GoogleMapActivity extends AppCompatActivity {
+import java.util.List;
+
+public class GoogleMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient; //device location
+    private PlacesClient placesClient; //
+    private List<AutocompletePrediction> predictionList; //places list
+    private LocationCallback locationCallback; //if location is null
+
+    private MaterialSearchBar materialSearchBar;
+    private View mapView;
+    private Button btnSetLocation;
+
+    private final float DEFAULT_ZOOM = 18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_map);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        materialSearchBar = findViewById(R.id.searchBar);
+        btnSetLocation = findViewById(R.id.button_set_location);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+        mapFragment.getMapAsync(this);
+        mapView = mapFragment.getView();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true); //current location button will  be shown
+
+        /**by default location button align in top*/
+        //bottom a align korar jonno below code
+        if (mapView!=null && mapView.findViewById(Integer.parseInt("1"))!=null){
+           View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP,0);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,RelativeLayout.TRUE);
+            layoutParams.setMargins(0,0,45,180);
+        }
     }
 }
