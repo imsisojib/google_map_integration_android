171857ed35399ed640423494e9dfa2fa224ee367
Md Sirajul Islam Sojib <sirajulislam1123@gmail.com>
2020-12-10 17:42:00 +0600

    runtime permissions added with basic GoogleMapActivity.

--- app/src/main/java/com/imsisojib/googlemapintegration/MainActivity.java
+++ app/src/main/java/com/imsisojib/googlemapintegration/MainActivity.java
@@ -1,8 +1,25 @@
 package com.imsisojib.googlemapintegration;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +27,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.button_permission).setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                gotoGoogleMapActivity();
+            }else{
+                Dexter.withContext(MainActivity.this)
+                        .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                                gotoGoogleMapActivity();
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                                if (permissionDeniedResponse.isPermanentlyDenied()){
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                            builder.setTitle("Permission Denied")
+                                            .setMessage("Permission to access device location is permanently denied. " +
+                                                    "You need to go to setting to allow the permission.")
+                                            .setNegativeButton("Cancel",null)
+                                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    Intent intent = new Intent();
+                                                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                                    intent.setData(Uri.fromParts("package",getPackageName(),null));
+
+                                                }
+                                            }).show();
+                                }
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                Toast.makeText(MainActivity.this, "Permission is Denied.", Toast.LENGTH_SHORT).show();
+                            }
+                        }).check();
+            }
+        });
+
     }
+
+    private void gotoGoogleMapActivity() {
+        Intent intent = new Intent(MainActivity.this,GoogleMapActivity.class);
+        startActivity(intent);
+    }
+
 }
